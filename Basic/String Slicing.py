d56937dccb3a347112ca7263c35b864b1b361220
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2022-09-05 07:52:36 +0530

    string slicing added

---
+++ Basic/String Slicing.py
@@ -0,0 +1,6 @@
+name = 'srinivas'
+# if I want to get first characters of that name
+print(name[0:2])
+# [start : end : step]  here start is inclusive and end is exclusive
+# lets do reverse
+print(name[::-1])
