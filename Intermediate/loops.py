8510cb08a2ee3a0b3740c447b47d0b9a6c6b56bf
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2022-09-09 19:05:26 +0530

    Loops are added

---
+++ Intermediate/loops.py
@@ -0,0 +1,23 @@
+# basic example regarding loops
+a = 0
+# while loop
+while a < 10:   # while loops are used when you've no idea how many times to loop
+    print(a)
+    a += 1
+
+# in while loop it iterates only when the statement is true
+
+"""Quick Quiz"""
+# write a program ro print 1 to 50 using a while loop
+print("\n")  # it is not mandatory
+num = 1
+while num <= 50:
+    print(num)
+    num += 1
+
+# let's discuss infinite loop
+#  is nothing but a loop which iterates repeatedly
+# let's create
+# num1 = 1
+# while num1 <= 5:
+#     print(num1)
