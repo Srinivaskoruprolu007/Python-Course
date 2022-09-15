e4d649fb83176d84b1b99c0055bf4569b9b3dbe3
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2022-09-16 00:24:05 +0530

    Modified

--- Data Structures/Dictionary Functions.py
+++ Data Structures/Dictionary Functions.py
@@ -1,29 +1,29 @@
 oxford = {"gift": "something willingly given to someone to appreciate", "this": "A keyword in java", "Youtube": "A video sharing platform ", "Instagram": "A picture sharing platform"}
 
-# # return a list of (key, value) tuples
-# print(oxford.items())
+# return a list of (key, value) tuples
+print(oxford.items())
 
-# # return a list containing dictionary keys
-# print(oxford.keys())
+# return a list containing dictionary keys
+print(oxford.keys())
 
-# # return a list containing dictionary values
-# print(oxford.values())
+# return a list containing dictionary values
+print(oxford.values())
 
-# # update the dictionary with key-value pair
-# oxford.update({"friend": "vasu"})
-# print(oxford)
-# oxford.update({"friend": "vasu", "vasu": "good boy"})
-# print(oxford)
-# # let's print values by for loop
-# for key in oxford.keys():
-#     print(key)
+# update the dictionary with key-value pair
+oxford.update({"friend": "vasu"})
+print(oxford)
+oxford.update({"friend": "vasu", "vasu": "good boy"})
+print(oxford)
+# let's print values by for loop
+for key in oxford.keys():
+    print(key)
 
-# # let's print keys and values by using for loop
-# for key, values in oxford.items():
-#     print(key, "==", values)
+# let's print keys and values by using for loop
+for key, values in oxford.items():
+    print(key, "==", values)
 
-# # get function will give value of that particular key in the dictionary
-# print(oxford.get("friend"))
+# get function will give value of that particular key in the dictionary
+print(oxford.get("friend"))
 
-for values in oxford.values():
-    print(values)
+for i in oxford.values():
+    print(i)
