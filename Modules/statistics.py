02e36f9becbbe2e5d4e97258e4536e25ebf711ee
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2022-09-16 20:46:20 +0530

    modules are added

---
+++ Modules/statistics.py
@@ -0,0 +1,6 @@
+from statistics import * # importing statistics module
+ages = [20, 20, 4, 24, 25, 22, 26, 20, 23, 22, 26]
+print(mean(ages))
+print(median(ages))
+print(mode(ages))
+print(stdev(ages))
